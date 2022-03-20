11fe6c5b6ecfb18f6daa8b6b8d197e39bef1c6db
maciej.wawryczuk@ziflow.com <Maciekschybaty01>
2022-03-20 19:33:30 +0100

    3 działające testy

--- cypress/integration/inpost.feature
+++ cypress/integration/inpost.feature
@@ -1,12 +1,17 @@
-Feature: Śledzenie paczek na stronie głónej InPost
+Feature: Śledzenie paczek na stronie głównej InPost
 
-  Scenario Outline: Powienien znaleść paczkę dla wybranego numeru
+  Scenario: Powienien pokazać status "Paczka Dostarczona"
     Given Otwieram główną stronę Inpost
-    When Wpisuję <numer> paczki
-    Then Paczka zostaje znaleziona /
+    When Wpisuję numer 695010139244330118871532 paczki
+    Then Paczka dostarczona
 
-    Examples:
-| numer |
-|695010139244330118871532|
-|614300139276200016725507|
-|566002139228300128891811|
+  Scenario: Powienien pokazać status "Paczka przygotowana przez Nadawcę"
+    Given Otwieram główną stronę Inpost
+    When Wpisuję numer 614300139276200016725507 paczki
+    Then Paczka przygotowana przez Nadawcę
+
+  Scenario: Nie powienien znaleść paczki dla wybranego numeru
+    Given Otwieram główną stronę Inpost
+    When Wpisuję numer 566002139228300128891811 paczki
+    Then Paczka nie zostaje znaleziona
+  
--- cypress/support/step_definitions/inpost-steps.js
+++ cypress/support/step_definitions/inpost-steps.js
@@ -1,20 +1,41 @@
-import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
+import {Before, Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
+
+Before(()=> {
+    cy.visit("https://inpost.pl/");
+    cy.get('#onetrust-accept-btn-handler').as('zaakceptuj');
+    cy.get('.alert--component > .close').as('covid');
+    cy.get('.inputcontainer > .form--control').as('numer');
+    cy.get('.hero--form--container > .btncontainer > .btn--primary').as('znajdz');
+})
 
 Given('Otwieram główną stronę Inpost', () => {
-    cy.visit("https://inpost.pl/");
-    cy.get('#onetrust-accept-btn-handler').click();
-    cy.get('.alert--component > .close').click()
+    cy.url().should('include', 'inpost');
+    cy.get('@zaakceptuj').click();
+    cy.get('@covid').click();
 })
 
-When("Wpisuję {word} paczki", (numer) => {
-    cy.get('.inputcontainer > .form--control').type(numer)
-    cy.get('.hero--form--container > .btncontainer > .btn--primary').click()
+When("Wpisuję numer {word} paczki", (numer) => {
+    cy.get('@numer').type(numer).then(function (linkText) {
+        cy.log("Szukam po wybranym numerze" + linkText.text())
+    });
+    cy.get('@znajdz').click();
 })
 
-Then('Paczka zostaje znaleziona {word}', (url) => {
-    cy.url()
-        .should('eq', `${Cypress.config().baseUrl}${url}`)
+Then('Paczka dostarczona', () => {
+    cy.get('.parcel--statuses--list').as('inPostResult');
+    cy.get('@inPostResult').should('contain.text', "Dostarczona");
+    cy.screenshot();
+})
+
+Then('Paczka przygotowana przez Nadawcę', () => {
+    cy.get('.parcel--statuses--list').as('inPostResult');
+    cy.get('@inPostResult').should('contain.text', "Przygotowana przez Nadawcę.");
+    cy.screenshot();
+})
 
         //W teście GUI powinien na ostatnim ekranie zapisać się screen 
-
+Then('Paczka nie zostaje znaleziona', () => {
+    cy.get('.parcel--statuses--list').as('inPostResult');
+    cy.get('@inPostResult').should('not.contain.text', "Dostarczona");
+    cy.screenshot();
 })
